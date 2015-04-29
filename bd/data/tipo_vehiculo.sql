ab4a64eac361ee507fbae68b61947c72fec936a4
Luis Hernandez <lfhernandezb@gmail.com>
2015-04-29 00:07:12 -0300

    marcas y modelos definitivos

--- bd/data/tipo_vehiculo.sql
+++ bd/data/tipo_vehiculo.sql
@@ -1,4 +1,2 @@
-INSERT INTO tipo_vehiculo (id_tipo_vehiculo, descripcion) VALUES (1,'AUTOMOVIL');
-INSERT INTO tipo_vehiculo (id_tipo_vehiculo, descripcion) VALUES (2,'FURGÓN');
-INSERT INTO tipo_vehiculo (id_tipo_vehiculo, descripcion) VALUES (3,'MINI BUS');
-INSERT INTO tipo_vehiculo (id_tipo_vehiculo, descripcion) VALUES (4,'MOTO');
+INSERT INTO tipo_vehiculo (ID_TIPO_VEHICULO, DESCRIPCION) VALUES (1,'AUTOMOVIL-CAMIONETA-FURGON-MINIBUS-MOTOR HOME-STATION WAGON-TODO TERRENO');
+INSERT INTO tipo_vehiculo (ID_TIPO_VEHICULO, DESCRIPCION) VALUES (2,'MOTO');
